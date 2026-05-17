705d119e2abe31c0f9b42ee059c72caac8e4883f
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-17 18:13:24 +0300

    some

--- Day2/server.js
+++ Day2/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 
+import { pooling } from './Db/Conndb.js';
 
 const app = express();
 
@@ -9,6 +10,36 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cors());
+
+
+app.post('/add-task',async(req,res)=>{
+    const {name} = req.body
+    try {
+        const newTask = await pooling.query(`
+            INSERT INTO TASKS (name) VALUES($1)
+            `,[name]);
+        res.send(newTask);
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+// ..getTsks
+
+app.get('/',async(req,res)=>{
+    try {
+        const getTasks = await pooling.query(
+            `
+            select * from tasks 
+            `
+        )
+        res.send(getTasks);
+    } catch (error) {
+        console.log(error)
+    }
+}
+);
 
 const port = process.env.PORT
 
