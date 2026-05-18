43a0fac642fc61653f878097352fe9161a7cb08c
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-18 13:31:44 +0300

    updte client

--- Day1/src/Context/AppContext.jsx
+++ Day1/src/Context/AppContext.jsx
@@ -1,10 +1,14 @@
+import { useEffect } from "react";
 import { createContext, useContext, useState } from "react";
+import toast from "react-hot-toast";
 
 const AppContext = createContext();
 
-export const AppContextProvider  = ({children}) =>{
+export const AppContextProvider  = ({children}) => {
 
     const [tasks,setTasks] = useState([
+    ]);
+    const [tasksData,setTasksData] = useState([
     ]);
 
 
@@ -12,13 +16,42 @@ const handleChange = (task)=>{
     setTasks()
 }
 
+const fetchTasks = async ()=>{
+        const res = await fetch('http://localhost:4000/get-tasks',{
+            method:"GET"
+        })
+        const data = await res.json();
+        setTasksData(data.data);
+        console.log(data.data);
+}
+
+useEffect(()=>{
+    fetchTasks();
+    console.log(tasksData)
+},[])
+
 //addTask 
-const addTask = (inputData,setOpenEdit)=>{
+const addTask = async(inputData,setOpenEdit)=>{
     if(inputData){
-        setTasks((prev)=>([...prev,{Task:inputData,id:tasks.length += 1}]))
+    setTasks((prev)=>([...prev,{Task:inputData,id:tasks.length += 1}]))
+    const res = await fetch("http://localhost:4000/add-task",{
+        method:"post",
+        headers:{"content-type"  : "application/json"},
+        body:JSON.stringify({name:inputData})
+    })
+    const data = await res.json();
+    if(data.success){
+    fetchTasks()
     }
     setOpenEdit(false)
 }
+    
+}
+
+
+
+
+
 
 //updateTask
 const TheupdateTask = (updating,updatTask,setOpenEdit)=>{
@@ -35,19 +68,25 @@ const updatedTask = tasks.map((task)=>{
 }
 
 //delete task
-const deleteTask = (id)=>{
-  setTasks(tasks.filter((task)=>task.id !== id));
-}
+const deleteTask = async(id)=>{
+  setTasks(tasksData.filter((task)=>task.id !== id));
+
+  const res = await fetch(`http://localhost:4000/delete-task/${id}`,{
+            method:"DELETE"
+        })
+        const data = await res.json();
+        toast.success(data.message);
+        fetchTasks();
+    }
 
     const value = {
         tasks,setTasks,addTask,
-        TheupdateTask,deleteTask
+        TheupdateTask,deleteTask,tasksData
     };
 
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
 }
-
 
 export const useAppContext =()=> useContext(AppContext);
